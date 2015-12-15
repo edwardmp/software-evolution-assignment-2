3dfa6785920b1296a6908b69941ea7ba3dd5ada9
Olav <olav.trauschke@student.uva.nl>
2015-12-15 12:17:24 +0100

    Finished working on standardize for statements, not yet tested. Also added a testcases containing a try-catch-finally block.

---
+++ visualizer/testCases/TryCatchFinallyTest.java
@@ -0,0 +1,16 @@
+public class TryCatchFinallyTest {
+	public void tryCatchFinallyTest() {
+		try {
+			System.out.println("test");
+		}
+		catch(Exception e) {}
+		finally {
+			System.out.println("test");
+			System.out.println("test");
+			System.out.println("test");
+			System.out.println("test");
+			System.out.println("test");
+			System.out.println("test");
+		}
+	}
+}
